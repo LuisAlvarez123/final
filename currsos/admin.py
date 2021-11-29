2c05194e576a43ca3f2c54ef01416f304d03d0dd
luis alvarez <luisjoa@mesoamericana.edu.gt>
2021-11-29 13:11:06 -0800

    creado modelo

--- currsos/admin.py
+++ currsos/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Register your models here. 
+class AlumnosAdmin(admin.ModelAdmin):
+    fields = ('nombre',)
+
+class CursosAdmin(admin.ModelAdmin):
+    fields = ('nombre',)
+
+class AsignacionAdmin(admin.ModelAdmin):
+    fields = ('nombre',)
+
+admin.site.register(Alumnos, AlumnosAdmin)
+admin.site.register(Cursos, CursosAdmin)
+admin.site.register(Asignacion, AsignacionAdmin)
--- currsos/models.py
+++ currsos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Alumnos(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Cursos(models.Model):
+    nombre = models.CharField(max_length=50)
+    alumnos = models.ManyToManyField(Alumnos, through='Asignacion')
+
+    def __str__(self):
+        return self.nombre
+
+class Asignacion(models.Model):
+    alumnos =  models.ForeignKey(Alumnos, on_delete=models.CASCADE)
+    cursos = models.ForeignKey(Cursos, on_delete=models.CASCADE)
+    fecha_asignacion = models.DateField()
